96950b38872569a31bd130415ad808829eddae41
Leon <leon.karho.chung@gmail.com>
2024-01-03 16:34:34 -0500

    create user profile route

--- app/Controllers/Http/UserProfileController.ts
+++ app/Controllers/Http/UserProfileController.ts
@@ -2,17 +2,21 @@
 import prisma from "../../../prisma/prisma";
 
 export default class UserProfileController {
-  async store({ request }) {
+  async create({ request, response }) {
+    const user = request.authenticatedUser;
     const { username, firstName, lastName } = request.body();
 
-    const newUser = await prisma.user.create({
-      data: { username, firstName, lastName },
+    const data = {
+      user_id: user.id,
+      username,
+      firstName,
+      lastName,
+    };
+
+    const newUserProfile = await prisma.userProfile.create({
+      data,
     });
 
-    return { newUser };
-  }
-
-  async index() {
-    return { msg: "this fetches all users" };
+    return { msg: "/users/profile/create", newUserProfile };
   }
 }
