e81bd3b2dae77a3bd8aa8aa9eea36fe93413e942
Leon <leon.karho.chung@gmail.com>
2024-01-03 15:45:32 -0500

    change User model to UserProfile

---
+++ prisma/migrations/20240103202932_change_user_model_name/migration.sql
@@ -0,0 +1,3 @@
+-- This is an empty migration.
+
+ALTER TABLE IF EXISTS "User" RENAME TO "UserProfile";
---
+++ prisma/migrations/20240103203155_change_user_model_name_to_userprofile/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "UserProfile" RENAME CONSTRAINT "User_pkey" TO "UserProfile_pkey";
