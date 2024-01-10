b5df1d178f5a3a5ca0636b19789d8d418f0af9aa
Leon <leon.karho.chung@gmail.com>
2024-01-10 14:43:15 -0500

    refactor to switch statement

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -60,10 +60,17 @@ export default class ExceptionHandler extends HttpExceptionHandler {
     }
 
     if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      if (error.code === "P2002") {
-        return response.status(400).json({
-          error: "User profile already exists",
-        });
+      switch (error.code) {
+        case "P2002":
+          return response.status(400).json({
+            error: "User profile already exist",
+          });
+        case "P2025":
+          return response.status(404).json({
+            error: "User profile does not exist",
+          });
+        default:
+          return response.status(500).json({ error: "Internal server error" });
       }
     }
   }
