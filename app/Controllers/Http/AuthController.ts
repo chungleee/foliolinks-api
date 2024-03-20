197310e18b6ab7d7109e4b756f5e4ba93896621c
Leon <leon.karho.chung@gmail.com>
2024-03-20 13:28:23 -0400

    remove endpoint, and fix register logic

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -24,8 +24,19 @@ export default class AuthController {
 
     if (error) response.send(error);
 
-    const { session } = data;
-    response.cookie("foliolinks_auth", session);
+    const { session, user } = data;
+
+    const access_token = session?.access_token;
+    const refresh_token = session?.refresh_token;
+
+    const userData = {
+      id: user?.id,
+      email: user?.email,
+      role: user?.role,
+    };
+
+    response.cookie("foliolinks_auth_refresh_token", refresh_token);
+    return { user: userData, access_token };
   }
 
   async login({ request, response }) {
@@ -68,28 +79,11 @@ export default class AuthController {
     }
 
     const { session } = data;
-    console.log("new session: ", session);
 
     const access_token = session?.access_token;
     const new_refresh_token = session?.refresh_token;
 
     response.cookie("foliolinks_auth_refresh_token", new_refresh_token);
     return { access_token };
-  }
-
-  async whoisthis({ request, response }) {
-    const bearerToken = request.header("authorization");
-
-    if (!bearerToken?.startsWith("Bearer ")) {
-      throw new Error("Unauthorized");
-    }
-
-    const access_token = bearerToken?.split(" ")[1];
-
-    const { error } = await supabase.auth.getUser(access_token);
-
-    if (error) {
-      return { error };
-    }
   }
 }
