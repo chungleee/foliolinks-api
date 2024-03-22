6480c9f9ac6ae64218f8fece1e285fbc69059e94
Leon <leon.karho.chung@gmail.com>
2024-03-22 13:56:41 -0400

    fix domain

--- config/app.ts
+++ config/app.ts
@@ -102,7 +102,7 @@ export const http: ServerConfig = {
   |--------------------------------------------------------------------------
   */
   cookie: {
-    domain: "",
+    domain: ".leonchung.ca",
     path: "/",
     maxAge: "2h",
     httpOnly: true,
